18ebf24747c630301badad9cb17bc7bf557b402e
Christian Chimezie <chris.chimezie@gmail.com>
2020-08-25 16:41:01 -0400

    q1: create grid coordinate iterator

--- q1/super_word_search.py
+++ q1/super_word_search.py
@@ -1,18 +1,24 @@
-def _hashGrid(grid, wrap):
-	"""Return hash table with entries
-	in format: {0-0: letter1, 0-1: letter2, ...}
-	for all coordinates in the grid."""
-	h = {}
+def coordinates(grid):
+	"""Iterate through N by M grid from left to right, top to bottom."""
 	N, M = len(grid), len(grid[0])
 	for i in range(N):
 		for j in range(M):
 			coor = (i, j)
-			hashable_coor = '-'.join(str(idx) for idx in coor)  # tuple cannot be hashed (used as key)
-			h[hashable_coor] = grid[i][j]
+			yield coor
+
+def _letterHash(grid):
+	"""
+	Return hash table with entries in format:
+	{0-0: letter1, 0-1: letter2, ...} for all coordinates in grid.
+	"""
+	h = {}
+	for coor in coordinates(grid):
+		i, j = coor
+		# tuple cannot be hashed (used as key)
+		hashable_coor = '-'.join(str(idx) for idx in coor)
+		h[hashable_coor] = grid[i][j]
 	return h
 
 def search(grid, word, wrap=False):
-	"""Search grid for word using specified
-	wrap mode. Return boolean."""
-	coor = _hashGrid(grid=grid, wrap=wrap)
-	return coor
+	"""Search grid for word using specified wrap mode."""
+	return _letterHash(grid)
