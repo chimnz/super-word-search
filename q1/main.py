e869b22f401c81c25f2cc0bd4570f81c878e0893
Christian Chimezie <chris.chimezie@gmail.com>
2020-08-26 12:52:17 -0400

    q1: change terminal seperator

--- q1/main.py
+++ q1/main.py
@@ -11,10 +11,10 @@ WORDS = config['words']
 
 def main():
 	wrap = True if WRAP_MODE == 'WRAP' else False
-	half_terminal_width = int(os.popen('stty size', 'r').read().split()[1]) // 2  # https://stackoverflow.com/a/943921
-	seperator = '\n' + '*' * half_terminal_width
+	terminal_width = int(os.popen('stty size', 'r').read().split()[1])  # https://stackoverflow.com/a/943921
+	seperator = "-" * terminal_width
 
-	print(TITLE, seperator)
+	print(TITLE + '\n' + seperator)
 	for word in WORDS:
 		results = search(GRID, word, wrap)
 		if results:
@@ -22,9 +22,10 @@ def main():
 			for stack in results:
 				start, end = stack[0], stack[-1]
 				msg = "=> {} TO {}".format(start, end)
-				print(msg, seperator)
+				print(msg)
 		else:
 			print(word, ": NOT FOUND")
+		print(seperator)
 
 if __name__ == "__main__":
 	main()
